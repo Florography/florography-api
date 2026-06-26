f23af4c3dc779ab95224e23a72ea66c2b2b272d5
olvchae <eunchae1405@gmail.com>
2026-06-26 16:19:51 +0900

    기본

---
+++ src/main/java/com/korit/florographyapi/FlorographyApiApplication.java
@@ -0,0 +1,13 @@
+package com.korit.florographyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlorographyApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlorographyApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/korit/florographyapi/FlorographyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.korit.florographyapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlorographyApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
