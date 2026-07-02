28c9863b6f5a8e8865d8f2ddba568243a678d035
olvchae <eunchae1405@gmail.com>
2026-07-02 11:30:31 +0900

    no message

--- src/main/java/com/korit/florographyapi/User/dto/UserReqCreate.java
+++ src/main/java/com/korit/florographyapi/User/dto/UserReqCreate.java
@@ -1,4 +1,14 @@
 package com.korit.florographyapi.User.dto;
 
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
 public class UserReqCreate {
+    private String nickname;
+    private String email;
+    private String profileImage;
+    private LocalDate createdAt;
+    private Long daysConnected;
 }
