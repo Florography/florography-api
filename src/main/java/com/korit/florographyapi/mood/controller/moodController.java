dc5c8438b75640398d062be7facd21a4821decac
ginye28 <tsna1268@gmail.com>
2026-07-07 09:32:06 +0900

    0707/create/mood 구현

--- src/main/java/com/korit/florographyapi/mood/controller/moodController.java
+++ src/main/java/com/korit/florographyapi/mood/controller/moodController.java
@@ -1,11 +1,9 @@
 package com.korit.florographyapi.mood.controller;
 
-import com.korit.florographyapi.ShareBoard.service.CommentService;
 import com.korit.florographyapi.dto.ApiResponse;
 import com.korit.florographyapi.mood.service.moodService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
